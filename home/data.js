4e24f534410af655c9d91dc9fc23d20c3bd67afa
cohuuuuuuuu <cohuuuuuuuu@gmail.com>
2024-12-05 10:52:11 +0800

    vr

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-home",
+      "name": "home",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4718623028789626,
+        "pitch": -0.00881016815658242,
+        "fov": 0.750705561589459
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3088342920268587,
+          "pitch": 0.12557259939613807,
+          "title": "<br>",
+          "text": "電腦歷史博物館（英語：Computer History Museum，縮寫：CHM）是以電腦為主題的博物館，位在美國加州山景城"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ super/data.js
@@ -0,0 +1,254 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "電腦博物館-超級電腦入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.9849434206259708,
+        "pitch": 0.2250443808809024,
+        "fov": 1.0728003200462808
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6577069461944012,
+          "pitch": 0.665319880163656,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.050688156653977856,
+          "pitch": 0.6264484757922908,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6600172555616943,
+          "pitch": 0.2521491677154515,
+          "title": "",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "電腦博物館-超級電腦入口左",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6309266755807235,
+          "pitch": 0.9472318178597234,
+          "rotation": 5.497787143782138,
+          "target": "0--"
+        },
+        {
+          "yaw": -0.19102168867782865,
+          "pitch": 0.7242224757634563,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -2.998024704214407,
+          "pitch": 0.8187217999430363,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3470872171793893,
+          "pitch": 0.15840853134690036,
+          "title": "超級電腦",
+          "text": "<div style=\"text-align: justify;\">由Gene Amdahl操刀的IBM 7030 ″ Stretch ″（1961年）是電腦工業史第一台導入指令管線的電腦。</div>"
+        }
+      ]
+    },
+    {
+      "id": "2--",
+      "name": "電腦博物館-超級電腦入口左前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.819866275574359,
+          "pitch": 1.0240400421442217,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.47067228857959975,
+          "pitch": 0.3777042904731722,
+          "title": "",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3--",
+      "name": "電腦博物館-超級電腦入口左後",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.118529645797782,
+          "pitch": 0.7992848238935526,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1567767880067574,
+          "pitch": 0.2674075923928676,
+          "title": "",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4--",
+      "name": "電腦博物館-超級電腦入口後",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.050527510514989,
+          "pitch": 0.49953364261311783,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.778703941672319,
+          "pitch": 0.3966213413507802,
+          "title": "<br>",
+          "text": "麻省理工學院實驗室儀器計算機"
+        }
+      ]
+    }
+  ],
+  "name": "super",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
